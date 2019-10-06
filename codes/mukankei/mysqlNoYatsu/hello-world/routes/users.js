43592cdb64e0d34db98fd633145d1ca69a8cf3b8
matuzakikirigaya <kirigayakazuto8888w@gmail.com>
2019-10-07 08:40:24 +0900

    移した

---
+++ codes/mukankei/mysqlNoYatsu/hello-world/routes/users.js
@@ -0,0 +1,7 @@
+var express = require("express");
+export var router = express.Router();
+
+/* GET users listing. */
+router.get("/", function(req, res, next) {
+  res.send("respond with a resource");
+});
